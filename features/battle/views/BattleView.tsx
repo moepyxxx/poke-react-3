29259e6b2e30b22896a1fe15ef36dfbd75b0e46b
moeko-iwai <moepyxxxxxx@gmail.com>
2023-12-08 19:04:14 +0900

    refactor: コードの見通しをよくする

--- features/battle/views/BattleView.tsx
+++ features/battle/views/BattleView.tsx
@@ -3,96 +3,9 @@
 import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { BattlePokemon } from "../types";
-import { v4 as uuid } from "uuid";
 import { useReadyBattle } from "../hooks/useReadyBattle";
 import { Battle } from "../components/Battle";
 import { onHandPokemonsAtom } from "@/atoms/onHandPokemons";
-
-const SAMPLE_ON_HAND_POKEMONS: BattlePokemon[] = [
-  {
-    basic: {
-      pokemonUId: uuid(),
-      name: "アチャモ",
-      level: 10,
-      imageURL:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/255.png",
-    },
-    status: {
-      hp: {
-        max: 20,
-        remain: 15,
-      },
-    },
-    works: [
-      {
-        id: 1,
-        name: "たいあたり",
-        pp: {
-          max: 10,
-          remain: 10,
-        },
-      },
-      {
-        id: 2,
-        name: "なきごえ",
-        pp: {
-          max: 10,
-          remain: 10,
-        },
-      },
-      {
-        id: 3,
-        name: "すなかけ",
-        pp: {
-          max: 10,
-          remain: 10,
-        },
-      },
-      {
-        id: 4,
-        name: "ひのこ",
-        pp: {
-          max: 10,
-          remain: 10,
-        },
-      },
-    ],
-  },
-];
-
-const SAMPLE_ENEMY_POKEMON: BattlePokemon = {
-  basic: {
-    pokemonUId: uuid(),
-    name: "ジグザグマ",
-    level: 3,
-    imageURL:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/263.png",
-  },
-  status: {
-    hp: {
-      max: 10,
-      remain: 10,
-    },
-  },
-  works: [
-    {
-      id: 1,
-      name: "たいあたり",
-      pp: {
-        max: 10,
-        remain: 10,
-      },
-    },
-    {
-      id: 2,
-      name: "なきごえ",
-      pp: {
-        max: 10,
-        remain: 10,
-      },
-    },
-  ],
-};
 
 export const BattleView = () => {
   const [onHandPokemons] = useAtom(onHandPokemonsAtom);
@@ -111,9 +24,11 @@ export const BattleView = () => {
     setBattlePokemon(pokemon);
   };
 
-  return battlePokemon == null ? (
-    ""
-  ) : (
+  if (battlePokemon == null) {
+    return <></>;
+  }
+
+  return (
     <Battle
       enemyPokemon={battlePokemon}
       onHandPokemon={setOnBattlePokemon(onHandPokemons[0])}

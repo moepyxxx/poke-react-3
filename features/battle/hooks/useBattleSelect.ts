1c640e66530a98e379de7f1a648d31f51898c3d2
moeko-iwai <moepyxxxxxx@gmail.com>
2023-12-09 12:36:11 +0900

    update: selectのカーソルがUIと同じ動きをするようにした

--- features/battle/hooks/useBattleSelect.ts
+++ features/battle/hooks/useBattleSelect.ts
@@ -21,11 +21,45 @@ export const useBattleSelect = ({
 }: Options) => {
   const [nextSelect, setNextSelect] = useState<ActionSelect | null>(null);
 
+  /**
+   * cursorは上下左右に移動する
+   * 1 2
+   * 3 4
+   * 選択肢は以上の順で並んでいるためそれを制御する
+   * @returns
+   */
   const onCursorUp = () => {
     if (nextSelect == null) {
       return;
     }
-    if (nextSelect.workId === 1) {
+    if (nextSelect.workId <= 2) {
+      return;
+    }
+    setNextSelect({
+      ...nextSelect,
+      workId: nextSelect.workId - 2,
+    });
+  };
+  const onCursorDown = () => {
+    if (nextSelect == null) {
+      return;
+    }
+    if (nextSelect.workId >= 3) {
+      return;
+    }
+    if (nextSelect.workId + 2 > worksCount) {
+      return;
+    }
+    setNextSelect({
+      ...nextSelect,
+      workId: nextSelect.workId + 2,
+    });
+  };
+  const onCursorLeft = () => {
+    if (nextSelect == null) {
+      return;
+    }
+    if (nextSelect.workId % 2 === 1) {
       return;
     }
     setNextSelect({
@@ -33,11 +67,14 @@ export const useBattleSelect = ({
       workId: nextSelect.workId - 1,
     });
   };
-  const onCursorDown = () => {
+  const onCursorRight = () => {
     if (nextSelect == null) {
       return;
     }
-    if (nextSelect.workId === worksCount) {
+    if (nextSelect.workId % 2 === 0) {
+      return;
+    }
+    if (nextSelect.workId + 1 > worksCount) {
       return;
     }
     setNextSelect({
@@ -61,6 +98,12 @@ export const useBattleSelect = ({
     }
     if (action?.action === "onPushBelow") {
       onCursorDown();
+    }
+    if (action?.action === "onPushLeft") {
+      onCursorLeft();
+    }
+    if (action?.action === "onPushRight") {
+      onCursorRight();
     }
   });
 

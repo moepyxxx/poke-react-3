df0f14231a8416b0191b79cf0e49f036af0a7102
moeko-iwai <moepyxxxxxx@gmail.com>
2023-12-14 00:16:25 +0900

    update: baseが指定できるようにした

--- packages/tools/app/features/fieldMap/EditFieldMap.tsx
+++ packages/tools/app/features/fieldMap/EditFieldMap.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { FC } from "react";
-import { FieldObjectMap } from "@types";
+import { FieldObjectMap, ShortFieldPosition } from "@types";
 import {
   DndContext,
   useDraggable,
@@ -12,8 +12,21 @@ import {
 type Props = {
   fieldObjectMap: FieldObjectMap;
 };
-export const EditFieldMap: FC<Props> = ({ FieldObjectMap }) => {
-  //   const ids = ["hoge", "fuga", "piyo"];
+export const EditFieldMap: FC<Props> = () => {
+  const fields = ["black", "load", "grass"];
+  const maps: ShortFieldPosition[] = ["1-1", "1-2", "1-3"];
+
+  const [fieldObjectMap, setFieldObjectMap] = React.useState<FieldObjectMap>({
+    "1-1": {
+      base: "black",
+    },
+    "1-2": {
+      base: "black",
+    },
+    "1-3": {
+      base: "black",
+    },
+  });
 
   return (
     <DndContext
@@ -22,30 +35,54 @@ export const EditFieldMap: FC<Props> = ({ FieldObjectMap }) => {
         console.log(event);
       }}
       onDragEnd={(event) => {
-        const { over } = event;
+        const { over, active } = event;
         console.log(over);
-        if (over == null) {
+        if (over == null || active.data.current == null) {
           return;
         }
-        console.log(over.id);
+        console.log(
+          active.data.current.field,
+          "is over [ droppable field data ]"
+        );
+        console.log(over.id, "is over [ graggable id ]");
+        setFieldObjectMap({
+          ...fieldObjectMap,
+          [over.id]: {
+            base: active.data.current.field,
+          },
+        });
       }}>
-      <div className="border-black border-2">
-        <Droppable id="sample" />
-        <div>
-          <Draggable id="hoge">hoge</Draggable>
+      <div className="border-black border-2 p-2">
+        <div className="flex">
+          {maps.map((map, index) => (
+            <Droppable key={`droppable_${index}`} id={map}>
+              {fieldObjectMap[map].base}
+            </Droppable>
+          ))}
+        </div>
+        <p className="ml-3 mt-2">Base</p>
+        <div className="flex">
+          {fields.map((field, index) => (
+            <BaseDraggable id={field} key={`draggable_${index}`}>
+              {field}
+            </BaseDraggable>
+          ))}
         </div>
       </div>
     </DndContext>
   );
 };
 
-type DraggableProps = {
+type BaseDraggableProps = {
   children: React.ReactNode;
   id: string;
 };
-const Draggable: React.FC<DraggableProps> = ({ children, id }) => {
+const BaseDraggable: React.FC<BaseDraggableProps> = ({ children, id }) => {
   const { setNodeRef, listeners, attributes, transform } = useDraggable({
     id: id,
+    data: {
+      field: id,
+    },
   });
 
   const transformStyle = transform
@@ -68,10 +105,10 @@ const Draggable: React.FC<DraggableProps> = ({ children, id }) => {
 };
 
 type DroppableProps = {
-  //   children: React.ReactNode;
+  children: React.ReactNode;
   id: string;
 };
-export const Droppable: FC<DroppableProps> = ({ id }) => {
+export const Droppable: FC<DroppableProps> = ({ id, children }) => {
   const { setNodeRef, isOver } = useDroppable({
     id,
   });
@@ -80,7 +117,11 @@ export const Droppable: FC<DroppableProps> = ({ id }) => {
     <div
       ref={setNodeRef}
       className="w-40 h-40 border-black border-2 m-3 p-2 cursor-pointer">
+      {id}
+      <br />
       {isOver ? "over" : "not over"}
+      <br />
+      {children}
     </div>
   );
 };

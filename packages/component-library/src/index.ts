daa334c87abec85723e9a5ca30e66a1a8e30dd63
moeko-iwai <moepyxxxxxx@gmail.com>
2024-01-04 18:35:38 +0900

    update: exportした

--- packages/component-library/src/index.ts
+++ packages/component-library/src/index.ts
@@ -1,2 +1,5 @@
 export * from "./Button/Button";
 export * from "./TextLink/TextLink";
+export * from "./Title/Title";
+export * from "./Typography/Typography";
+export * from "./form/TextField/TextField";
